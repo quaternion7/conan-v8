8754c24f7a443d673130929357f09ae791fec927
Leonardo Trocato <22778911+quaternion7@users.noreply.github.com>
2018-08-08 18:17:11 -0300

    Updated conanfile (still not functional)

--- conanfile.py
+++ conanfile.py
@@ -1,46 +1,20 @@
 from conans import ConanFile, CMake, tools
 
-
 class V8Conan(ConanFile):
     name = "v8"
     version = "7.0.174"
-    license = "MIT"
+    license = "https://github.com/v8/v8/blob/master/LICENSE"
     url = "https://github.com/quaternion7/conan-v8"
     description = "Conan recipe for embedding Google's V8 Javascript Engine."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = "shared=True"
     generators = "cmake"
 
-    def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="hello")
-        cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.h", dst="include", keep_path=True)
         self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.bin", dst=".", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
-
+        self.cpp_info.libs = self.collect_libs()
